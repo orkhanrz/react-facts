b86656bc7d3178ef2acb7e0edff28bd4517ce8f3
Orkhan Rzali <orkhan.rz8@gmail.com>
2021-11-17 00:45:40 +0400

    icon issue fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import icon from "../images/react.svg";
 
-export default function Header(){
-    return (
-        <header>
-            <img src="../images/react.svg" alt="icon"/>
-            <h3>React Facts</h3>
-            <h4>React Course - Project 1</h4>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header>
+      <img src={icon} alt="icon" />
+      <h3>React Facts</h3>
+      <h4>React Course - Project 1</h4>
+    </header>
+  );
 }
